6bc36cbca661d163b21d7b99238bed7363afdde3
Gi Silva <112526312+gisferreira394@users.noreply.github.com>
2023-05-24 22:19:33 -0300

    .

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+//operadores racionais
+
+echo (20> 30) . " False <br>"; // maior
+echo (20<30) . "True <br>"; // menor
+echo (10 == 10 ). "True<br>"; //igualdade
+echo (10 != 10) . "False<br>"; //diferente
+echo (10>= 10) . "true<br>"; //maior ou igual
+echo (10<=10)  . "true<br>"; //menor ou igual
+
+//operadores lógicos
+echo (10 ==10 && 10 >5) . "true <br>"; //Operador E
+echo (10 == 10 || 10> 5)  . "True <br>"; //Operador ou
+echo (!false) . "False <br>"; // operador não
+?>
+
+
+</body>
+</html>
